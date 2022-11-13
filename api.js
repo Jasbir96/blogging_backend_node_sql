e97aa36231452c5bf4f9dcc60d59ce045fdc5f8c
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-13 12:40:14 +0530

    added 404 page not found route handler

--- api.js
+++ api.js
@@ -1,5 +1,11 @@
 const express=require("express");
 const app=express();
+app.use(function(req,res){
+res.status(404).json({
+    status:"failure",
+    message:"no matching route found"
+})
+})
 app.listen(3000,()=>{
     console.log("listening on port 3000");
 })
