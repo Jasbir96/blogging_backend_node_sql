e228a48c7da656a4bfea3eed5aadec898f702eb7
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 12:05:47 +0530

    created structure for handler  for signup user

--- api.js
+++ api.js
@@ -1,11 +1,23 @@
 const express=require("express");
 const app=express();
-app.use(function(req,res){
-res.status(404).json({
-    status:"failure",
-    message:"no matching route found"
+
+app.post("/users", function (req, res) {
+    console.log("req body data", req.body);
+    res.status(200).json({
+        status: "success",
+        message: "user created successfully"
+    })
 })
+// 404 page not found  
+app.use(function (req, res) {
+    return res.status(404).json({
+        status: "failure",
+        message: "no matching route found"
+    })
 })
+
+
+
 app.listen(3000,()=>{
     console.log("listening on port 3000");
 })
