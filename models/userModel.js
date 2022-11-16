fb19f7b51d37f69ab9b0287004b1e47ad8fe878c
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 12:38:55 +0530

    createded password hash

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,12 @@
 const connection=require("../connection");
 const { v4: uuidv4 } = require('uuid');
+const bcrypt=require('bcrypt');
 const create=async (userObj)=>{
 // 1. data transformation
 // a.) generate id  
     userObj.id = uuidv4();
 //b .) generate password hash
+userObj["password_hash"]=bcrypt.hash(userObj.password,10);
 //c.) remove password confirm Password,password -> after we have created hash
 // 2. make a db query
 
