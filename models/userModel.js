1b766a9d2b2490838877da071a42b930a6b87a26
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 12:35:17 +0530

    chore (npm):npm i uuid

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,9 @@
 const connection=require("../connection");
-const create=async ()=>{
+const { v4: uuidv4 } = require('uuid');
+const create=async (userObj)=>{
 // 1. data transformation
 // a.) generate id  
+
 //b .) generate password hash
 //c.) remove password confirm Password,password -> after we have created hash
 // 2. make a db query
