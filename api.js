30dae6c989d0dd740bf9c6431d27a27b1f6418fc
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 12:22:42 +0530

    created pesudo code for user creation

--- api.js
+++ api.js
@@ -1,13 +1,35 @@
 const express=require("express");
 const app=express();
 app.use(express.json());
-app.post("/users", function (req, res) {
-    console.log("req body data", req.body);
-    res.status(200).json({
-        status: "success",
-        message: "user created successfully"
-    })
+app.post("/users", userSignupController)
+
+async function userSignupController(req, res) {
+    try{
+// 1. check for every data that is required is present
+let userObj=req.body;
+if(userObj.username==undefined||userObj.password==undefined||userObj.email==undefined||
+    userObj.confirmPassword==undefined||userObj.bio==undefined){
+        res.status(400).json({
+            status:"failure",
+            message:"missing required data"
+        })
+    } 
+//  2. send the data to DB -> to create user
+    await userModel.create(userObj);
+// 3. send success response to the client  
+res.status(201).json({
+    status:"success",
+    message:"user created successfully"
 })
+    }catch(err){
+        res.status(500).json({
+            status:"failure",
+            err:err.message
+        })
+    }
+}
+
+
 // 404 page not found  
 app.use(function (req, res) {
     return res.status(404).json({
