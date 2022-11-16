9376b9c9463d18df387affdd6c41089a82ed05ec
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 12:35:37 +0530

    generated uuid for use's table

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const create=async (userObj)=>{
 // 1. data transformation
 // a.) generate id  
-
+    userObj.id = uuidv4();
 //b .) generate password hash
 //c.) remove password confirm Password,password -> after we have created hash
 // 2. make a db query
