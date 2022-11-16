9a4c5666e3da30f1a3af77ecacafa85648e0f4b5
Jasbir <mr.jasbirsingh96@gmail.com>
2022-11-16 12:13:17 +0530

    added expres.json to accept data from http req

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-
+app.use(express.json());
 app.post("/users", function (req, res) {
     console.log("req body data", req.body);
     res.status(200).json({
